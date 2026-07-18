7d6ee6eda619d39628ef875686e2af03036f7ef0
iknouee <toddjames4569@gmail.com>
2026-07-18 03:12:27 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,24 +11,31 @@ const commands = [
 
     new SlashCommandBuilder()
         .setName('panic')
-        .setDescription('Lock every channel in the server.')
+        .setDescription('Lock every channel.')
         .toJSON(),
-
 
     new SlashCommandBuilder()
         .setName('unpanic')
-        .setDescription('Unlock every channel in the server.')
+        .setDescription('Unlock every channel.')
+        .toJSON(),
+
+    new SlashCommandBuilder()
+        .setName('vibe')
+        .setDescription('Get a random Beloved vibe.')
         .toJSON(),
 
 ];
 
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' })
+    .setToken(process.env.TOKEN);
 
 
 (async () => {
+
     try {
-        console.log('Registering slash commands...');
+
+        console.log("Registering slash commands...");
 
 
         await rest.put(
@@ -42,9 +49,12 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         );
 
 
-        console.log('Successfully registered slash commands.');
+        console.log("Successfully registered slash commands.");
 
     } catch (error) {
+
         console.error(error);
+
     }
+
 })();
