89c0ce59f1ef2ed60ae4ad0f2e9d505d17b3ab56
iknouee <toddjames4569@gmail.com>
2026-08-23 02:36:46 +0100

    Update testwelcome.js

--- src/commands/moderation/testwelcome.js
+++ src/commands/moderation/testwelcome.js
@@ -42,11 +42,13 @@ module.exports = {
 
         try {
             const embed = new EmbedBuilder()
-                .setColor("#ff8fc7")
-                .setTitle("welcome to beloved 💗")
+                .setColor("#000000")
+                .setTitle("welcome to beloved 🖤")
                 .setDescription(
                     `hey ${interaction.user}, welcome to **Beloved**\n\n` +
-                    `glad to have you here, make yourself at home <3`
+                    `before you get started, make sure you verify so you can access the server.\n\n` +
+                    `head over to <#1499900580431396987> and type **verify**\n\n` +
+                    `welcome to BLVD <3`
                 )
                 .setImage(
                     "https://cdn.discordapp.com/attachments/1540882634568368139/1540883508845613128/C2A8B411-7B4A-48DF-A4E2-EA5F623A6D86.png?ex=6a8b9318&is=6a8a4198&hm=4e4e92bd7cf0f81515776527bf61417bad17bd30ce8a89abe839c5382d7413fd"
